4b5ccec0dda749d537650a7c8ce87e39d5dfc105
AdMaven <akshathad998@gmail.com>
2024-03-09 14:01:19 +0530

    1st commit in this project

---
+++ src/test/java/Sample/Sample_one.java
@@ -0,0 +1,17 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Sample_one {
+
+	
+	@Test
+	public void sampleone() {
+		System.out.println("---------sampleone-----------");
+	}
+	
+	@Test
+	public void sampleTwo() {
+		System.out.println("--------sampleTwo---------------");
+	}
+}
